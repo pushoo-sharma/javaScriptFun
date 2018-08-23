dba76a43c32395ef42d53fb1f89f7f82910cafbc
Pushpak Sharma <sharmatryhard@gmail.com>
2018-08-23 16:30:22 +0530

    wrap up !

--- lists-conditionals--01-conditional-content/src/App.js
+++ lists-conditionals--01-conditional-content/src/App.js
@@ -5,29 +5,35 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id : '1',name: 'Max', age: 28 },
+      { id : '2',name: 'Manu', age: 29 },
+      { id : '3',name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
   
-  nameChangedHandler = ( event ) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id === id;
+    });
+    const person = {
+    ...this.state.persons[personIndex]
+    };
+    //const person = Object.assign({},this.state.persons[personIndex]);
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState( {persons : persons} );
   }
 
   deletePersonHandler = (personIndex)=>{
-    const person = this.state.persons;
+    //const person = this.state.persons
+    //const person = this.state.persons.slice();
+    const person = [...this.state.persons];
     person.splice(personIndex,1);
-    this.setState({persons:persons});
+    this.setState({persons:person});
 
   }
 
@@ -53,8 +59,11 @@ class App extends Component {
           {this.state.persons.map((person,index) => {
             return <Person
             click = { () => this.deletePersonHandler(index)}
+            key = {person.id}
             name = {person.name}
-            age = {person.age}/>
+            age = {person.age}
+            changed = {(event) => this.nameChangedHandler(event,person.id)}
+            />
           })}
         </div>
       );
