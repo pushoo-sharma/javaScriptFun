e411ba8995151f8d733165cfbc6ecf434bfbc754
Pushpak Sharma <sharmatryhard@gmail.com>
2018-08-24 11:11:53 +0530

    commit

--- lists-conditionals--01-conditional-content/src/App.js
+++ lists-conditionals--01-conditional-content/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium,{StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -44,11 +45,16 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color : 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover' : {
+        backgroundColor : "lightgreen",
+        color : "black"
+      }
     };
 
     let persons = null;
@@ -67,20 +73,34 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor : "lightred",
+        color : "black"
+      }
+    };
+    //let classes = ['red','bold'].join(' ');
+    const classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
     }
-
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+    }
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default Radium(App);
