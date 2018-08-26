74d4753d69de0a91163430c8a8b049a15a1151b6
Pushpak Sharma <sharmatryhard@gmail.com>
2018-08-26 10:56:10 +0530

    Learning react using tutorials :)

---
+++ hello-react/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <h2>React</h2>
+        <p data-myattribute = "somevalue">I am learning react :)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
